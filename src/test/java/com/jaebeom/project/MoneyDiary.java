5f68cd619068f0d166e819ea8acc55c5a36f083f
beumyjs <beumyjs@naver.com>
2018-10-01 21:57:16 +0900

    ignore add

---
+++ src/test/java/com/jaebeom/project/MoneyDiary.java
@@ -0,0 +1,18 @@
+package com.package
+import java.time.LocalDateTime;
+import java.lang.String;
+
+public class MoneyDiary{
+    LocalDateTime 날짜;
+    String 내용;
+    int 수입;
+    int 지출;
+    int 잔액;
+
+    public static void main(String[] args){
+        MoneyDiary moneyDiary = new MoneyDiary();
+
+        System.out.print(moneyDiary.잔액);
+    }
+
+}
